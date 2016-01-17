1eb6160d158073836177ea0a353256f6932f49a8
FabioAng1 <fabioang@live.it>
2016-01-17 10:25:34 +0100

    modifica file get-events

--- Implementazione/index.php
+++ Implementazione/index.php
@@ -2,9 +2,14 @@
 <?php
 session_start();
 if(isset($_SESSION['ut'])&& isset($_SESSION['pw'])&& ( strlen($_SESSION['ut'])>0 ) && ( strlen($_SESSION['pw'])>0 )){
-header("location:login.php");
-}else{
-?>
+	if(strcmp(substr($matricola,0,2),"17")==0){//amministratori->17
+		//$myPassword = sha1(mysqli_real_escape_string($password));
+		header('Location: admin.php');
+	}else{
+		header('Location: calendario.php');
+	}
+}
+	?>
 <html>
 	<head>
 		<title>TIME PLATFORM</title>
@@ -59,4 +64,3 @@ header("location:login.php");
 	</div>
 </body>
 </html>
-<?php }//fine else?>
--- Implementazione/login.php
+++ Implementazione/login.php
@@ -1,53 +1,50 @@
 <?php
-
-$connessione=false;
-if(isset($_SESSION['ut'])&&isset($_SESSION['pw'])){
-	$matricola=$_SESSION['ut'];
-	$password=$_SESSION['pw'];
+session_start();
+if(isset($_SESSION['ut'])&& isset($_SESSION['pw'])&& ( strlen($_SESSION['ut'])>0 ) && ( strlen($_SESSION['pw'])>0 )){
 	if(strcmp(substr($matricola,0,2),"17")==0){//amministratori->17
-												//$myPassword = sha1(mysqli_real_escape_string($password));
-												header('Location: admin.php');
-												}else{
-													  header('Location: calendario.php');
-														}
-}else {
-
-	if ((isset($_POST['matricola']) && isset($_POST['psw']))) {
-
-		include('gestoreAuth.php');
-		include('utente.php');
-		$matricola = $_POST['matricola'];
-		$password = $_POST['psw'];
-
-		$utente = new utente($matricola, $password);
-		$gestoreAuth = new gestoreAuth();
+		//$myPassword = sha1(mysqli_real_escape_string($password));
+		header('Location: admin.php');
+	}else{
+		header('Location: calendario.php');
+	}
+}
+else {
 
 
-		$tipo = $gestoreAuth->login($utente);
+		if ((isset($_POST['matricola']) && isset($_POST['psw']))) {
+
+			include('gestoreAuth.php');
+			include('utente.php');
+			$matricola = $_POST['matricola'];
+			$password = $_POST['psw'];
+
+			$utente = new utente($matricola, $password);
+			$gestoreAuth = new gestoreAuth();
+			$tipo = $gestoreAuth->login($utente);
 
 
-		if (strcmp($tipo, "false") != 0) {
-			if (strcmp($tipo, "admin") == 0) {
-				if (isset($_SESSION['ut']) && isset($_SESSION['pw'])) {
-					header('Location: admin.php');
-				} else {
-					$gestoreAuth->logout();
+			if (strcmp($tipo, "false") != 0) {
+				if (strcmp($tipo, "admin") == 0) {
+					if (isset($_SESSION['ut']) && isset($_SESSION['pw'])) {
+						header('Location: admin.php');
+					} else {
+						$gestoreAuth->logout();
+					}
 				}
-			}
-			if (strcmp($tipo, "autista") == 0) {
-				if (isset($_SESSION['ut']) && isset($_SESSION['pw'])) {
-					header('Location: calendario.php');
-				} else {
-					echo "logOut calendario";
-					$gestoreAuth->logout();
+				if (strcmp($tipo, "autista") == 0) {
+					if (isset($_SESSION['ut']) && isset($_SESSION['pw'])) {
+						header('Location: calendario.php');
+					} else {
+						echo "logOut calendario";
+						$gestoreAuth->logout();
+					}
 				}
+			} else {
+				$gestoreAuth->logout();
 			}
 		} else {
-			$gestoreAuth->logout();
-		}
-	} else {
-		// echo "logout post";
-		header('Location:logout.php');
+			// echo "logout post";
+			header('Location:logout.php');
 	}
 }
 ?>
